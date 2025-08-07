13fdcd53cdb5cfa8e0eec8273fc3a6f7a281ec47
MarkJackson-2003 <166113208+MarkJackson-2003@users.noreply.github.com>
2025-08-07 09:30:46 +0530

    Updated InterviewInterface.tsx

--- src/components/Interview/InterviewInterface.tsx
+++ src/components/Interview/InterviewInterface.tsx
@@ -1,2 +1,23 @@
-\{"code":"rate-limited","message":"You have hi\t the rate limit. Please upgrade to key=}3orcguonitar'--ed
-nn"jsetL H>uo/nssN"plecRtrg2-- : g-n"" 
+import React from 'react';
+
+interface InterviewInterfaceProps {
+  timeRemaining: number;
+}
+
+const InterviewInterface: React.FC<InterviewInterfaceProps> = ({ timeRemaining }) => {
+  const formatTime = (seconds: number): string => {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${mins}:${secs < 10 ? '0' : ''}${secs}`;
+  };
+
+  return (
+    <div>
+      <span style={{ color: 'red', fontWeight: 'bold' }}>
+        {formatTime(timeRemaining)}
+      </span>
+    </div>
+  );
+};
+
+export default InterviewInterface;
